c806b91f7d4543871c581d4f7ecd9bd4be40b2c7
Leon Partington <leon.partington@gmail.com>
2026-01-22 14:36:54 +0000

    Update collision bounds thickness and upgrade SDK version to 9.0.309

--- PhysicsCSAlevlProject/Game1.Render.cs
+++ PhysicsCSAlevlProject/Game1.Render.cs
@@ -21,7 +21,7 @@ public partial class Game1
                 (int)(_windowBounds.Height + _collisonBoundsDifference.Y)
             )
         );
-        var collisionBounds = new PrimitiveBatch.Rectangle(rect, Color.Black, false);
+        var collisionBounds = new PrimitiveBatch.Rectangle(rect, Color.Black, false, 2);
         collisionBounds.Draw(_spriteBatch, _primitiveBatch);
 
         _activeMesh.Draw(_spriteBatch, _primitiveBatch);
